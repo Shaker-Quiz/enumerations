d340fba452e791b219ceb8023bbba5fdca906558
yurkimus <yurkimus@gmail.com>
2024-11-30 02:39:36 +0100

    Adjust exports config

--- source/constants.js
+++ source/constants.js
@@ -1,6 +1,9 @@
-export let Constants = /** @type {const} */ ({
+/**
+ * @type {Constants}
+ */
+export let Constants = {
   /**
    * @see https://www.rfc-editor.org/rfc/rfc9562.html#name-nil-uuid
    */
   NilUuid: '00000000-0000-0000-0000-000000000000',
-})
+}
---
+++ source/index.js
@@ -0,0 +1,5 @@
+export * from './constants.js'
+export * from './minio.js'
+export * from './neo4j.js'
+export * from './postgres.js'
+export * from './regexps.js'
