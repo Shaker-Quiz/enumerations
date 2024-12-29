8b2cc6b4234c77d10bdc1f399a15179dc7996e81
yurkimus <yurkimus@gmail.com>
2024-12-29 23:28:50 +0100

    Add City and Cities features

--- source/services.js
+++ source/services.js
@@ -1,16 +1,187 @@
 export let Features = {
+  City: 'City',
+  Cities: 'Cities',
+
   CityPublic: 'CityPublic',
   CitiesPublic: 'CitiesPublic',
+
   GamePublic: 'GamePublic',
   GamesPublic: 'GamesPublic',
 }
 
 export let Transformers = {
-  [Features.CityPublic]: body => body,
+  [Features.City]: body => ({
+    name: body.name,
+    id: body.id,
+    time_created: body.time_created,
+    time_updated: body.time_updated,
+    price: body.price,
+    currency: body.currency,
+    game_time: body.game_time,
+    timezone: body.timezone,
+    vk_link: body.vk_link,
+    vk_comment: body.vk_comment,
+    tg_link: body.tg_link,
+    tg_login: body.tg_login,
+    tg_password: body.tg_password,
+    tg_comment: body.tg_comment,
+    inst_link: body.inst_link,
+    inst_login: body.inst_login,
+    inst_password: body.inst_password,
+    inst_comment: body.inst_comment,
+    is_franchise: body.is_franchise,
+    alias: body.alias,
+    vk_group_id: body.vk_group_id,
+    chatapp_line: body.chatapp_line,
+    chatapp_user: body.chatapp_user,
+    chatapp_tag: body.chatapp_tag,
+    chatapp_category: body.chatapp_category,
+    telegram_chat_id: body.telegram_chat_id,
+    title: body.title,
+    description: body.description,
+    region: body.region,
+    country: body.country,
+    is_default: body.is_default,
+    yandex_metrica: body.yandex_metrica,
+    chatapp_legacy: body.chatapp_legacy,
+    phone: body.phone,
+    email: body.email,
+    address: body.address,
+
+    analitycs: {
+      custom_html: body.custom_html,
+      custom_script: body.custom_script,
+      yandex_metrica: body.yandex_metrica,
+    },
+
+    meta: {
+      title: body.meta_title,
+      description: body.meta_description,
+    },
+
+    participants: {
+      maximum: body.max_members_count,
+      minimum: body.min_members_count,
+    },
+  }),
 
   [Features.CitiesPublic]: body =>
     body
-      .map(item => item)
+      .map(body => ({
+        name: body.name,
+        id: body.id,
+        time_created: body.time_created,
+        time_updated: body.time_updated,
+        price: body.price,
+        currency: body.currency,
+        game_time: body.game_time,
+        timezone: body.timezone,
+        vk_link: body.vk_link,
+        vk_comment: body.vk_comment,
+        tg_link: body.tg_link,
+        tg_login: body.tg_login,
+        tg_password: body.tg_password,
+        tg_comment: body.tg_comment,
+        inst_link: body.inst_link,
+        inst_login: body.inst_login,
+        inst_password: body.inst_password,
+        inst_comment: body.inst_comment,
+        is_franchise: body.is_franchise,
+        alias: body.alias,
+        vk_group_id: body.vk_group_id,
+        chatapp_line: body.chatapp_line,
+        chatapp_user: body.chatapp_user,
+        chatapp_tag: body.chatapp_tag,
+        chatapp_category: body.chatapp_category,
+        telegram_chat_id: body.telegram_chat_id,
+        title: body.title,
+        description: body.description,
+        region: body.region,
+        country: body.country,
+        is_default: body.is_default,
+        yandex_metrica: body.yandex_metrica,
+        chatapp_legacy: body.chatapp_legacy,
+        phone: body.phone,
+        email: body.email,
+        address: body.address,
+
+        analitycs: {
+          custom_html: body.custom_html,
+          custom_script: body.custom_script,
+          yandex_metrica: body.yandex_metrica,
+        },
+
+        meta: {
+          title: body.meta_title,
+          description: body.meta_description,
+        },
+
+        participants: {
+          maximum: body.max_members_count,
+          minimum: body.min_members_count,
+        },
+      }))
+      .sort((a, b) => a.name.localeCompare(b.name)),
+
+  [Features.CityPublic]: body => ({
+    alias: body.city_alias,
+    id: body.city_id,
+    name: body.city_name,
+    vk_group_id: body.city_vk_group_id,
+    vk_link: body.city_vk_link,
+    tg_link: body.city_tg_link,
+    inst_link: body.city_inst_link,
+    phone: body.city_phone,
+    email: body.city_email,
+    address: body.city_address,
+
+    analitycs: {
+      custom_html: body.city_custom_html,
+      custom_script: body.city_custom_script,
+      yandex_metrica: body.city_yandex_metrica,
+    },
+
+    meta: {
+      title: body.city_meta_title,
+      description: body.city_meta_description,
+    },
+
+    participants: {
+      maximum: body.city_max_members_count,
+      minimum: body.city_min_members_count,
+    },
+  }),
+
+  [Features.CitiesPublic]: body =>
+    body
+      .map(body => ({
+        alias: body.city_alias,
+        id: body.city_id,
+        name: body.city_name,
+        vk_group_id: body.city_vk_group_id,
+        vk_link: body.city_vk_link,
+        tg_link: body.city_tg_link,
+        inst_link: body.city_inst_link,
+        phone: body.city_phone,
+        email: body.city_email,
+        address: body.city_address,
+
+        analitycs: {
+          custom_html: body.city_custom_html,
+          custom_script: body.city_custom_script,
+          yandex_metrica: body.city_yandex_metrica,
+        },
+
+        meta: {
+          title: body.city_meta_title,
+          description: body.city_meta_description,
+        },
+
+        participants: {
+          maximum: body.city_max_members_count,
+          minimum: body.city_min_members_count,
+        },
+      }))
       .sort((a, b) => a.name.localeCompare(b.name)),
 
   [Features.GamePublic]: body => ({
@@ -78,6 +249,26 @@ export let Transformers = {
 export let Requests = new Map()
 
 Requests
+  .set(
+    Features.City,
+    {
+      fallback: () => null,
+      onfulfilled: Transformers[Features.City],
+      onrejected: reason => {
+        throw reason
+      },
+    },
+  )
+  .set(
+    Features.Cities,
+    {
+      fallback: () => null,
+      onfulfilled: Transformers[Features.Cities],
+      onrejected: reason => {
+        throw reason
+      },
+    },
+  )
   .set(
     Features.CityPublic,
     {
